ce4d0f191a8bd34ab196d9ff2e2afff1d6c5ead9
milenaalmazyan <ams007@mail.ru>
2023-01-18 02:15:28 +0300

    hm

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
             System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
             i++;
         }
-
         System.out.println("Задача 2");
         int s = 1;
         while ( s <= 10){
